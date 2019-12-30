1a9944c908749042f4318ed16ef9962023f3e90c
admin888 <admin@qq.com>
2019-12-30 15:57:34 +0800

    Add module weblogic-rmi-iiop

---
+++ weblogic-rmi-iiop/src/main/java/com/longofo/Client.java
@@ -0,0 +1,66 @@
+package com.longofo;
+
+import com.alibaba.fastjson.JSON;
+
+import javax.naming.*;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
+public class Client {
+    public final static String JNDI_FACTORY = "com.sun.jndi.cosnaming.CNCtxFactory";
+
+    public static void main(String[] args) throws NamingException, IOException, ClassNotFoundException {
+        InitialContext initialContext = getInitialContext("iiop://127.0.0.1:7001");
+        System.out.println(JSON.toJSONString(listAllEntries(initialContext), true));
+
+    }
+
+    private static Map listAllEntries(Context initialContext) throws NamingException {
+        String namespace = initialContext instanceof InitialContext ? initialContext.getNameInNamespace() : "";
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        System.out.println("> Listing namespace: " + namespace);
+        NamingEnumeration<NameClassPair> list = initialContext.list(namespace);
+        while (list.hasMoreElements()) {
+            NameClassPair next = list.next();
+            String name = next.getName();
+            String jndiPath = namespace + name;
+            HashMap<String, Object> lookup = new HashMap<String, Object>();
+            try {
+                System.out.println("> Looking up name: " + jndiPath);
+                Object tmp = initialContext.lookup(jndiPath);
+                if (tmp instanceof Context) {
+                    lookup.put("class", tmp.getClass());
+                    lookup.put("interfaces", tmp.getClass().getInterfaces());
+                    Map<String, Object> entries = listAllEntries((Context) tmp);
+                    for (Map.Entry<String, Object> entry : entries.entrySet()) {
+                        String key = entry.getKey();
+                        if (key != null) {
+                            lookup.put(key, entries.get(key));
+                            break;
+                        }
+                    }
+                } else {
+                    lookup.put("class", tmp.getClass());
+                    lookup.put("interfaces", tmp.getClass().getInterfaces());
+                }
+            } catch (Throwable t) {
+                lookup.put("error msg", t.toString());
+                Object tmp = initialContext.lookup(jndiPath);
+                lookup.put("class", tmp.getClass());
+                lookup.put("interfaces", tmp.getClass().getInterfaces());
+            }
+            map.put(name, lookup);
+
+        }
+        return map;
+    }
+
+    private static InitialContext getInitialContext(String url) throws NamingException {
+        Hashtable env = new Hashtable();
+        env.put(Context.INITIAL_CONTEXT_FACTORY, JNDI_FACTORY);
+        env.put(Context.PROVIDER_URL, url);
+        return new InitialContext(env);
+    }
+}
